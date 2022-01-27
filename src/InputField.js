188f79e9ddd1839deb21156fe4bd686a164ad3a4
saizdx2010 <saiprashanth02012001@gmail.com>
2022-01-27 16:15:55 +0700

    Used hook to handle input data

--- src/InputField.js
+++ src/InputField.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const InputField = () => {
+    const [name, setName] = useState('');
+
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
     return (
         <div>
-
-            <input name="name" type="text"></input>
+            <form>
+                <label htmlFor='name'>Name</label><br />
+                <input name="name" id='name' type="text" value={name} onChange={handleChange}></input>
+            </form>
         </div>
     )
 }
